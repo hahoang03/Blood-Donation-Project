f06457d9eaf532168b4f61fe0f8c7ceade37589f
nathang0147 <nganhthang147@gmail.com>
2023-10-12 15:20:25 +0700

    Helloo file 1

---
+++ Blood-Donation-Project/src/main/java/com/mycompany/blood/donation/project/BloodDonationProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.blood.donation.project;
+
+/**
+ *
+ * @author NThang
+ */
+public class BloodDonationProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
